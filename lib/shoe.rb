6ee0697dde21d853fc7e132f75516a0860a1d710
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-25 00:00:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,11 +6,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-  end
-  
-  def unique_brands
-    BRANDS.uniq
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
   end
 
   def cobble
